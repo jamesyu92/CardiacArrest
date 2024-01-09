b5bcd221bf1b5c4f20b83be1c5b37ed9be4c56b3
jamesyu92 <jamesyu92@yahoo.com>
2024-01-09 11:06:25 -0500

    Not a bad start. Implements a scrollable image. Next step is to add clickable buttons on top, and timer and END on the bottom

--- CardiacArrest/ViewController.swift
+++ CardiacArrest/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var scrollView: UIScrollView!
+    var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set the scrollView's frame to be the size of the screen
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        
+        // Set the contentSize to 2 times the height of the phone screen
+        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: UIScreen.main.bounds.height*1.5)
+        
+        view.addSubview(scrollView)
+        
+        // Add the image
+        image = UIImageView(image: UIImage(named: "CardiacArrest.jpg"))
+        image.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        image.contentMode = .scaleAspectFit
+        scrollView.addSubview(image)
     }
 
 
