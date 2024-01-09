dedff2bd33dd40658987548cb9d93a0df17acc73
jamesyu92 <jamesyu92@yahoo.com>
2024-01-09 14:38:27 -0500

    Added a bunch of buttons to the top of the screen. The next step is to adjust the starting y-axis of the flowchart, and add two additional buttons on the BOTTOM of the screen for start and finish

--- CardiacArrest/ViewController.swift
+++ CardiacArrest/ViewController.swift
@@ -12,12 +12,38 @@ class ViewController: UIViewController {
     var scrollView: UIScrollView!
     var image: UIImageView!
     
+    
+    @IBOutlet weak var CPR_Label: UILabel!
+    @IBOutlet weak var EPI_Label: UILabel!
+    @IBOutlet weak var LOG_Button: UIButton!
+    
+    @IBOutlet weak var CPR_Button: UIButton!
+    @IBOutlet weak var EPI_Button: UIButton!
+    @IBOutlet weak var SHOCK_Button: UIButton!
+    
+    @IBOutlet weak var CPR_EPI_SHOCK_Stack: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Cosmetic Updates
+        CPR_Label.layer.cornerRadius = 5
+        CPR_Label.layer.borderWidth = 1.0
+        CPR_Label.layer.borderColor = UIColor.black.cgColor
+        
+        EPI_Label.layer.cornerRadius = 5
+        EPI_Label.layer.borderWidth = 1.0
+        EPI_Label.layer.borderColor = UIColor.black.cgColor
+        
+        LOG_Button.layer.cornerRadius = 5
+        LOG_Button.layer.borderWidth = 1.0
+        LOG_Button.layer.borderColor = UIColor.black.cgColor
+        
+        CPR_Button.layer.cornerRadius = 5
+        EPI_Button.layer.cornerRadius = 5
+        SHOCK_Button.layer.cornerRadius = 5
         
         // Set the scrollView's frame to be the size of the screen
-        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: CPR_EPI_SHOCK_Stack.frame.maxY, width: view.frame.width, height: view.frame.height))
         
         // Set the contentSize to 2 times the height of the phone screen
         scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: UIScreen.main.bounds.height*1.5)
