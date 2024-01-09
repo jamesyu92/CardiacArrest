c7c470c28e970b54d97c7741f0711da63970e34f
jamesyu92 <jamesyu92@yahoo.com>
2024-01-09 15:30:27 -0500

    Gets the HT info from sister. Fix the starting position of the image. Starts implementing the timing and the log logic

--- CardiacArrest/ViewController.swift
+++ CardiacArrest/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     var scrollView: UIScrollView!
     var image: UIImageView!
     
-    
     @IBOutlet weak var CPR_Label: UILabel!
     @IBOutlet weak var EPI_Label: UILabel!
     @IBOutlet weak var LOG_Button: UIButton!
@@ -22,6 +21,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var SHOCK_Button: UIButton!
     
     @IBOutlet weak var CPR_EPI_SHOCK_Stack: UIStackView!
+    
+    @IBOutlet weak var Sound_Button: UIButton!
+    @IBOutlet weak var Total_Time_Label: UILabel!
+    @IBOutlet weak var Start_End_Button: UIButton!
+    
+    @IBOutlet weak var Bottom_Stack: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,19 +47,29 @@ class ViewController: UIViewController {
         EPI_Button.layer.cornerRadius = 5
         SHOCK_Button.layer.cornerRadius = 5
         
-        // Set the scrollView's frame to be the size of the screen
-        scrollView = UIScrollView(frame: CGRect(x: 0, y: CPR_EPI_SHOCK_Stack.frame.maxY, width: view.frame.width, height: view.frame.height))
+        Total_Time_Label.layer.cornerRadius = 5
+        Total_Time_Label.layer.borderWidth = 1.0
+        Total_Time_Label.layer.borderColor = UIColor.black.cgColor
         
-        // Set the contentSize to 2 times the height of the phone screen
-        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: UIScreen.main.bounds.height*1.5)
-        
-        view.addSubview(scrollView)
-        
-        // Add the image
+        // Define the image
         image = UIImageView(image: UIImage(named: "CardiacArrest.jpg"))
         image.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         image.contentMode = .scaleAspectFit
+        
+        // Set the scrollView's frame to be the size of the screen
+        // Height: Starting y-value of Bottom Stack - Ending y-value of Top Stack - 20.0 (Two times the 10.0 margin)
+        var scrollViewHeight = Bottom_Stack.frame.minY - CPR_EPI_SHOCK_Stack.frame.maxY - 20.0
+        
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: CPR_EPI_SHOCK_Stack.frame.maxY, width: view.frame.width, height: scrollViewHeight))
+        
+        // Set the contentSize to 2 times the height of the phone screen
+        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: image.frame.height)
+        
+        view.addSubview(scrollView)
         scrollView.addSubview(image)
+        
+        // Scroll View Background Color
+        scrollView.backgroundColor = UIColor.black
     }
 
 
